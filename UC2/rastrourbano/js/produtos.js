27d010c8430bfb8d18b2e1009e65e18858caa90e
Henrique inCode <henrincode@gmail.com>
2025-06-05 22:29:14 -0300

    realizado parte da automação da lista de produtos do site

--- UC2/rastrourbano/js/produtos.js
+++ UC2/rastrourbano/js/produtos.js
@@ -19,7 +19,30 @@ produtos = [{
     precoAntigo: 183.95,
     categorias: ["Feminino", "Vestido", "Vermelho"],
     imagem: "vestidos/001.webp"
+},{
+    id: 1004,
+    descricao: "Jaqueta Feminina Scuba Tecido Suede moda inverno",
+    preco: 65.90,
+    precoAntigo: 0,
+    categorias: ["Feminino", "Jaqueta", "Marrom"],
+    imagem: "jaquetas/001.webp"
+},{
+    id: 1005,
+    descricao: "Coturno Feminino Bota Tratorada Preto bota preta confortável",
+    preco: 59.90,
+    precoAntigo: 149.90,
+    categorias: ["Feminino", "bota", "preto"],
+    imagem: "sapatos/001.webp"
+},{
+    id: 1006,
+    descricao: "Bota Coturno Adventure Cano Curto Masculino Motoqueiro",
+    preco: 104.90,
+    precoAntigo: 0,
+    categorias: ["Masculino", "bota", "marrom"],
+    imagem: "sapatos/002.webp"
 }]
+
+// Funções
 
 function listarProdutos(filtro = "tudo") {
     const localInject = document.querySelector("#lista-produtos")
@@ -27,16 +50,18 @@ function listarProdutos(filtro = "tudo") {
     localInject.innerHTML = ""
     if(filtro == "tudo") {
         produtos.forEach(produto => localInject.innerHTML += `
+            <a href="/produto/${produto.id}">
             <div class="produto">
                 <div class="imagem-texto">
                     <img src="img/produtos/${produto.imagem}" alt="">
                     <p class="texto">${produto.descricao}</p>
                 </div>
                 <p class="preco-texto">
-                    <span class="preco-riscado">${produto.precoAntigo}</span>
-                    <span class="preco">${produto.preco}</span>
+                    <span class="preco-riscado">${produto.precoAntigo ? "R$ "+ produto.precoAntigo.toFixed(2).replace(".",",") : ""}</span>
+                    <span class="preco">R$ ${produto.preco.toFixed(2).replace(".",",")}</span>
                 </p>
             </div>
+            </a>
             `)
     }
 
