3458b6afaf8969df5410e6725aba38f1165e96e3
Henrique inCode <henrincode@gmail.com>
2025-06-10 22:18:50 -0300

    Adicionado dados de categorias

--- UC5/site/js/geral.js
+++ UC5/site/js/geral.js
@@ -1,27 +1,54 @@
-const produtos = [
-  {
-    id: 1001,
-    nome: "Bolo de aniversário MineCraft",
-    categorias: ["Bolo", "Aniversário", "Criança", "Temático", "MineCraft"],
-    capa: "img/foto1"
-  },
-  {
-    id: 1002,
-    descricao: "Bolo especial KitKat",
-    categorias: ["Bolo", "Aniversário", "Adulto"],
-    capa: "img/foto2"
-  },
-];
+// const produtos = [
+//   {
+//     id: 1001,
+//     nome: "Bolo de Aniversário",
+//     categorias: ["Bolo", "Aniversário"],
+//     capa: "bolo/bolo-001.jpg"
+//   },
+//   {
+//     id: 1002,
+//     descricao: "Bolo especial KitKat",
+//     categorias: ["Bolo", "Aniversário", "Adulto"],
+//     capa: "img/foto2"
+//   },
+// ];
+
+// aprender sobre status code http
+// https://www.httpstatus.com.br/
 
 const categorias = [
   {
     id: 1001,
-    nome: "Bolo",
-    capa: 1002,
+    nome: "Bolos de Aniversário",
+    capa: "misc/bolo-001",
   },{
     id: 1002,
-    nome: "Bolo Aniversário",
-    capa: 1001,
+    nome: "Bolos Infantis",
+    capa: "/misc/bolo-004",
+  },{
+    id: 1003,
+    nome: "Docinhos de Festa",
+    capa: "/misc/docefesta-001.jpg"
+  },{
+    id: 1004,
+    nome: "Bolos Tradicionais",
+    capa: "/misc/bolo-002.jpg"
+  },{
+    id: 1005,
+    nome: "Panificação",
+    capa: "/misc/pao-001.jpg"
+  },{
+    id: 1006,
+    nome: "Sobremesas",
+    capa: "/misc/sobremesa-001"
+  },{
+    id: 1007,
+    nome: "Bolos de Travessa",
+    capa: "/misc/bolo-003.jpg"
+  },{
+    id: 1008,
+    nome: "Pavês",
+    capa: "/misc/pave-001.jpg"
   }
 ];
 
