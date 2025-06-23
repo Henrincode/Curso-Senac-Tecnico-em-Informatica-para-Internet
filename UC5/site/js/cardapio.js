39d8d30dba0e7093788f58d18209cd1daad82f2f
Henrincode <spellctro@gmail.com>
2025-06-23 13:02:23 -0300

    Criado arquivo de dados para categorias

--- UC5/site/js/cardapio.js
+++ UC5/site/js/cardapio.js
@@ -1,15 +1,24 @@
-async function produtos() {
+const produtos = []
+
+carregarProdutos()
+
+async function carregarProdutos() {
   const response = await fetch("/data/produtos.json");
   const dados = await response.json();
+
+  produtos.push(...dados)
 
   const produtosLista = document.querySelector("#produtos-lista");
   produtosLista.innerHTML = ""
 
-  dados.forEach(produto => (produtosLista.innerHTML += 
+  produtos.forEach(produto => (produtosLista.innerHTML += 
     `<div class="produto">
     <img src="/img/cardapio/${produto.img}">
+    <div class="idp">#${produto.id}</div>
     <div class="idp">#${produto.id}</div>
     </div>`));
 }
 
-produtos()
+function categoria(id) {
+
+}
