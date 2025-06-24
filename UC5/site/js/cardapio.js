a34fd72cf2b5b0a8327b8fb2bc73ec15eb93b875
Henrique inCode <henrincode@gmail.com>
2025-06-23 21:46:33 -0300

    Criado conexão com arquivo categorias e criado funções para tratar os nomes das categorias nos produtos

--- UC5/site/js/cardapio.js
+++ UC5/site/js/cardapio.js
@@ -1,24 +1,43 @@
-const produtos = []
+const produtos = [];
+const categorias = [];
 
 carregarProdutos()
 
 async function carregarProdutos() {
-  const response = await fetch("/data/produtos.json");
-  const dados = await response.json();
+  // Carregar produtos
+  const responseProdutos = await fetch("/data/produtos.json");
+  const dadosProdutos = await responseProdutos.json();
+  produtos.push(...dadosProdutos);
 
-  produtos.push(...dados)
+  // Carregar categorias
+  const responseCategorias = await fetch("/data/categorias.json");
+  const dadosCategorias = await responseCategorias.json();
+  categorias.push(...dadosCategorias);
 
+  // Renderizar produtos
   const produtosLista = document.querySelector("#produtos-lista");
-  produtosLista.innerHTML = ""
+  produtosLista.innerHTML = "";
 
-  produtos.forEach(produto => (produtosLista.innerHTML += 
-    `<div class="produto">
-    <img src="/img/cardapio/${produto.img}">
-    <div class="idp">#${produto.id}</div>
-    <div class="idp">#${produto.id}</div>
-    </div>`));
+  produtos.forEach(produto => {
+    
+    const categoria = charMax(nomeCategoria(produto.categoria), 15)
+
+    produtosLista.innerHTML += `
+      <div class="produto">
+        <img src="/img/cardapio/${produto.img}">
+        <div class="idp">#${produto.id}</div>
+        <div class="idp">${categoria}</div>
+      </div>`;
+  });
 }
 
-function categoria(id) {
+function nomeCategoria(id) {
+  return categorias.find(categoria => categoria.id === id).nome
+}
 
+function charMax(string, max) {
+  if (string.length > max) {
+    return `${string.slice(0, max)}...`;
+  }
+  return string;
 }
