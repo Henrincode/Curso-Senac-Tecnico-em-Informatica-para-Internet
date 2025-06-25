62d61f51ecb8563afcb79868a42d914514e28ea1
Henrincode <spellctro@gmail.com>
2025-06-25 10:42:01 -0300

    deletado arquivos antigos

--- UC5/site/js/depoimentos.js
+++ UC5/site/js/depoimentos.js
@@ -2,11 +2,11 @@ carregarDepoimentos()
 
 async function carregarDepoimentos() {
     const response = await fetch("/data/depoimentos.json")
-    const dados = await response.json()
+    const depoimentos = await response.json()
     const lista = document.querySelector("#depoimentos-lista")
 
     lista.innerHTML = ""
-    dados.forEach(depoimento => lista.innerHTML += `
+    depoimentos.forEach(depoimento => lista.innerHTML += `
         <div class="depoimento">
             <img src="/img/depoimentos/${depoimento.img}" alt="" />
             <h3>${depoimento.nome}</h3>
